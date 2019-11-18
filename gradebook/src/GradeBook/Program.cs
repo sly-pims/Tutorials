103e5219d9f9e9506aeecde06512be6d6f85395d
Sylvain <sly.pims@gmail.com>
2019-11-18 13:17:17 +1300

    First commit

---
+++ gradebook/src/GradeBook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var grades = new List<double>(){12.7, 58.0, 16.3};
+            grades.Add(56.1);
+
+            var result = 0.0;
+            foreach (var grade in grades)
+            {
+                result += grade;
+            }
+            Console.WriteLine ($"The result is {result}.");
+
+            var avergage = result / grades.Count;
+            Console.WriteLine($"The average grade is {avergage:N1}.");
+ 
+            if (args.Length > 0)
+            {
+                Console.WriteLine($"Hello {args[0]}!"); //String interpolation
+            }
+            else
+            {
+                Console.WriteLine("Hello!");
+            }
+
+        }
+    }
+}
